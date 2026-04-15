b941c2b359c2f8c48a0c245d372c2e2776cc67bf
stephenhall03 <stephenhall03@gmail.com>
2026-04-15 00:44:09 -0500

    Added logic that would create a yard slips

---
+++ YardManagementSim/Models/YardSlot.cs
@@ -0,0 +1,12 @@
+﻿namespace YardManagementSim.Models
+{
+    public class YardSlot
+    {
+        public string Code { get; set; } = "";
+        public string Trailer { get; set; } = "";
+        public string LoadId { get; set; } = "";
+        public string Seal { get; set; } = "";
+        public string Notes { get; set; } = "";
+
+    }
+}
