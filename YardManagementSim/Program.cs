0abe6c6c0b933d27d3803e4844ed914c5070eac6
stephenhall03 <stephenhall03@gmail.com>
2026-04-29 00:39:36 -0500

    Added check in and check out method

--- YardManagementSim/Program.cs
+++ YardManagementSim/Program.cs
@@ -13,6 +13,13 @@ builder.Services.AddDbContext<YardDbContext>(options =>
 
 var app = builder.Build();
 
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<YardDbContext>();
+
+    db.Database.EnsureCreated();
+}
+
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
